61af4965e2c6572fd1dbf36eb871b84bd9040cd6
Jatin Foujdar <jatin321001@gmail.com>
2024-11-20 23:41:20 +0530

    Initial Commit

---
+++ MOE-Habits-App/MOE_Habits_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOE_Habits_AppApp.swift
+//  MOE-Habits-App
+//
+//  Created by jatin foujdar on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MOE_Habits_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
