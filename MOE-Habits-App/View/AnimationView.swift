50dc18a1b9a4cbc45dfba3cb49027e2649df6904
Jatin Foujdar <jatin321001@gmail.com>
2024-11-21 21:11:50 +0530

    animation json commit

---
+++ MOE-Habits-App/View/AnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationView.swift
+//  MOE-Habits-App
+//
+//  Created by jatin foujdar on 20/11/24.
+//
+
+import SwiftUI
+
+struct AnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationView()
+}
