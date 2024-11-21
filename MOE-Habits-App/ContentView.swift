5982b17065af1ea15f85a0069a0c90e42736488a
Jatin Foujdar <jatin321001@gmail.com>
2024-11-21 23:51:54 +0530

    animation commit

--- MOE-Habits-App/ContentView.swift
+++ MOE-Habits-App/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Go For")
+            Text("Better ")
+            Text("Habit")
+            Text("With")
+            Text("MOE")
         }
         .padding()
     }
--- MOE-Habits-App/View/AnimationView.swift
+++ MOE-Habits-App/View/AnimationView.swift
@@ -1,15 +1,72 @@
-//
-//  AnimationView.swift
-//  MOE-Habits-App
-//
-//  Created by jatin foujdar on 20/11/24.
-//
-
 import SwiftUI
 
 struct AnimationView: View {
+    @State private var showText = false
+    @State private var moveToTopLeft = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 35/255, green: 35/255, blue: 35/255)
+                .edgesIgnoringSafeArea(.all)
+
+         
+            VStack(alignment: .leading, spacing: 15) {
+                Text("GO FOR")
+                    .font(.system(size: 80))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .opacity(showText ? 1 : 0)
+                    .scaleEffect(moveToTopLeft ? 0.8 : 1)
+                    .offset(x: moveToTopLeft ? -70 : 0, y: moveToTopLeft ? -50 : 0)
+                    .animation(.easeOut(duration: 1), value: moveToTopLeft)
+                    .padding()
+
+                Text("Better")
+                    .font(.system(size: 70))
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+                    .opacity(showText ? 1 : 0)
+                    .scaleEffect(moveToTopLeft ? 0.8 : 1)
+                    .offset(x: moveToTopLeft ? -70 : 0, y: moveToTopLeft ? -100 : 0)
+                    .animation(.easeOut(duration: 1).delay(0.2), value: moveToTopLeft)
+                    .padding()
+
+                Text("Habit")
+                    .font(.system(size: 70))
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+                    .opacity(showText ? 1 : 0)
+                    .scaleEffect(moveToTopLeft ? 0.8 : 1)
+                    .offset(x: moveToTopLeft ? -70 : 0, y: moveToTopLeft ? -150 : 0)
+                    .animation(.easeOut(duration: 1).delay(0.4), value: moveToTopLeft)
+                    .padding()
+                Text("With")
+                    .font(.system(size: 70))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .opacity(showText ? 1 : 0)
+                    .scaleEffect(moveToTopLeft ? 0.8 : 1)
+                    .offset(x: moveToTopLeft ? -70 : 0, y: moveToTopLeft ? -200 : 0)
+                    .animation(.easeOut(duration: 1).delay(0.6), value: moveToTopLeft)
+                    .padding()
+                Text("MOE")
+                    .font(.system(size: 70))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .opacity(showText ? 1 : 0)
+                    .scaleEffect(moveToTopLeft ? 0.8 : 1)
+                    .offset(x: moveToTopLeft ? -70 : 0, y: moveToTopLeft ? -250 : 0)
+                    .animation(.easeOut(duration: 1).delay(0.8), value: moveToTopLeft)
+                    .padding()
+            }
+            
+            .onAppear {
+                showText = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    moveToTopLeft = true
+                }
+            }
+        }
     }
 }
 
